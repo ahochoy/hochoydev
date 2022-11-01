894a3f5f130f303c04f6b543128536c6f696c703
Andrew HoChoy <a.hochoy@gmail.com>
2022-11-01 12:14:47 -0700

    Breaking down carousel component and adding sizes

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,51 +1,51 @@
 import * as React from "react";
-import { useEffect, useRef, useState } from "react";
-import arrowRight from '../../images/arrow-right.svg';
-import arrowLeft from '../../images/arrow-left.svg';
+import { useLayoutEffect, useEffect, useRef, useState } from "react";
+import { gsap } from 'gsap';
+import { ControlButton } from "./ControlButton";
+import { CarouselCard } from "./CarouselCard";
 
-interface CarouselProps {
-    title: string;
-}
-
-export function Carousel({title}: CarouselProps ) {
+export function Carousel({makeSmall = false}) {
 
     const containerRef = useRef<HTMLDivElement>(null);
-    const numberOfItems = 3;
+
+    const numberOfItems = 5;
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    function goCarousel(direction: string) {
-        switch (direction) {
-            case 'previous':
-                if (currentIndex === 0) return;
-                setCurrentIndex(currentIndex - 1);
-                break;
-            case 'next':
-                if (currentIndex + 1 === numberOfItems) return;
-                setCurrentIndex(currentIndex + 1);
-                break;
-            default:
-                break;
-        }
+    function goToNext() {
+        if (currentIndex + 1 === numberOfItems) return;
+        setCurrentIndex(currentIndex + 1);
+    }
+
+    function goToPrevious() {
+        if (currentIndex === 0) return;
+        setCurrentIndex(currentIndex - 1);
     }
 
     useEffect(() => {
         if (containerRef.current) {
             const gap = parseInt(getComputedStyle(containerRef.current).gap);
-            const itemWidth = containerRef.current.querySelectorAll('div').item(0).clientWidth
-            containerRef.current.style.transform = `translateX(-${currentIndex * (itemWidth + gap)}px)`
+            const itemWidth = containerRef.current.querySelectorAll('div').item(0).getBoundingClientRect().width
+
+            gsap.set(containerRef.current, {translateX: -currentIndex * (itemWidth + gap)})
         }
     }, [currentIndex])
+
+    const carouselSize = makeSmall ? "w-1/3" : "w-1/2"
     
     return <>
-        <div className="container mx-auto my-16 px-8 2xl:px-48 flex items-center relative">
-            <h4 className="z-50 font-thin text-blue-700 w-[13.5rem] text-right absolute -ml-32">{title}</h4>
-            <div className="flex w-full gap-8 relative transition-transform duration-300 ease-in-out" ref={containerRef}>
-                <div className="shrink-0 w-full aspect-[3/2] bg-red-600"></div>
-                <div className="shrink-0 w-full aspect-[3/2] bg-green-600"></div>
-                <div className="shrink-0 w-full aspect-[3/2] bg-blue-600"></div>
+        <div className="bg-lime-100">
+            <div className={`flex items-center relative bg-pink-300 container mx-auto ${carouselSize}`}>
+                <div className="flex w-full gap-8 relative transition-transform duration-300 ease-in-out" ref={containerRef}>
+                    <CarouselCard />
+                    <CarouselCard />
+                    <CarouselCard />
+                    <CarouselCard />
+                </div>
+                <div>
+                    <ControlButton clickHandler={goToPrevious} active={currentIndex !== 0} reverse />
+                    <ControlButton clickHandler={goToNext} active={currentIndex + 1 !== numberOfItems} />
+                </div>
             </div>
-            {(currentIndex !== 0) && <button onClick={() => goCarousel('previous')} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold p-4 rounded-full absolute -bottom-9 left-12"><img src={arrowLeft} width="40" alt="Previous" /></button>}
-            {(currentIndex + 1 !== numberOfItems) && <button onClick={() => goCarousel('next')} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold p-4 rounded-full absolute -bottom-9 right-12"><img src={arrowRight} width="40" alt="Next" /></button>}
         </div>
     </>
 }
