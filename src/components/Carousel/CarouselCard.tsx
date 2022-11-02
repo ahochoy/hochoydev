93a3f981734ea91cbcd261d232e95f6f4e737b52
Andrew HoChoy <a.hochoy@gmail.com>
2022-11-02 14:13:07 -0700

    Adding card details

--- src/components/Carousel/CarouselCard.tsx
+++ src/components/Carousel/CarouselCard.tsx
@@ -1,5 +1,11 @@
 import * as React from "react";
 
 export function CarouselCard() {
-    return <div className="shrink-0 w-full aspect-[3/2] bg-slate-300 opacity-50"></div>
+    return <div className="shrink-0 w-full aspect-[3/2] bg-gradient-to-tr from-blue-500 to-blue-900 rounded-xl flex overflow-hidden">
+        <div className="self-end w-full p-10 bg-gradient-to-tr from-zinc-900 to-transparent text-zinc-200">
+            <p className="w-fit mb-4 small-text uppercase rounded-full border-solid border px-3 border-zinc-200">Content Type</p>
+            <p className="heading bold-weight">This is an Item Title</p>
+            <p className="subheading">This is a blurb about the item, something splashy</p>
+        </div>
+    </div>
 }
