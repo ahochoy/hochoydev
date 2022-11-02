aa65c731c344d9a71dccd68b0215fa3546785d68
Andrew HoChoy <a.hochoy@gmail.com>
2022-11-02 12:14:31 -0700

    Make carousels loop

--- src/components/Carousel/ControlButton.tsx
+++ src/components/Carousel/ControlButton.tsx
@@ -5,18 +5,15 @@ import arrowLeft from '../../images/arrow-left.svg';
 interface ControlButtonProps {
     clickHandler(): void;
     reverse?: boolean;
-    active: boolean;
 }
 
-export function ControlButton({clickHandler, reverse = false, active}: ControlButtonProps) {
+export function ControlButton({clickHandler, reverse = false}: ControlButtonProps) {
 
     const buttonPosition = reverse ? "left-0 -translate-x-1/2" : "right-0 translate-x-1/2"
-    const buttonActive = active ? "opacity-100 hover:bg-gray-400" : "opacity-50"
 
     return <button 
         onClick={() => clickHandler()}
-        disabled={ !active } 
-        className={`bg-gray-300 drop-shadow-md text-gray-800 font-bold p-4 rounded-full absolute -translate-y-1/2 ${buttonPosition} ${buttonActive}`}>
+        className={`bg-gray-300 drop-shadow-md text-gray-800 font-bold scale-50 lg:scale-100 p-4 rounded-full absolute -translate-y-1/2 ${buttonPosition}`}>
             <img src={reverse ? arrowLeft : arrowRight} width="40" alt={reverse ? "Previous" : "Next"} />
         </button>
 }
