0432d348fc67774cd7bf947d2e47fafcd8f10026
Andrew HoChoy <a.hochoy@gmail.com>
2022-11-04 17:07:53 -0700

    Reduce text size on carousel cards

--- src/components/Carousel/CarouselCard.tsx
+++ src/components/Carousel/CarouselCard.tsx
@@ -4,8 +4,8 @@ export function CarouselCard() {
     return <div className="shrink-0 w-full aspect-[3/2] bg-gradient-to-tr from-blue-500 to-blue-900 rounded-xl flex overflow-hidden">
         <div className="self-end w-full p-10 bg-gradient-to-tr from-zinc-900 to-transparent text-zinc-200">
             <p className="w-fit mb-4 small-text uppercase rounded-full border-solid border px-3 border-zinc-200">Content Type</p>
-            <p className="heading bold-weight">This is an Item Title</p>
-            <p className="subheading">This is a blurb about the item, something splashy</p>
+            <p className="subheading bold-weight">This is an Item Title</p>
+            <p >This is a blurb about the item, something splashy</p>
         </div>
     </div>
 }
