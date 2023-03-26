6e640cb3fcce8aa0ceb05555db028e35dd365d3c
Andrew HoChoy <a.hochoy@gmail.com>
2023-03-25 18:23:02 -0700

    visual tweaks to text and spacing

--- src/components/Works/PastWork.tsx
+++ src/components/Works/PastWork.tsx
@@ -2,15 +2,15 @@ import * as React from "react";
 
 export function PastWork() {
     return <div className="w-full space-y-4">
-        <p className="text-2xl font-bold leading-tight tracking-tighter uppercase">Past Work</p>
-        <p>Companies I've had the pleasure of working for</p>
+        <p className="heading font-bold leading-tight tracking-tighter uppercase">Past Work</p>
+        <p className="subheading">Companies I've had the pleasure of working for</p>
         <ul className="flex gap-4">
             <li>HR Software</li>
             <li>Global Consultancy</li>
             <li>Global Digital Agency</li>
             <li>HR Software</li>
         </ul>
-        <p>Selected clients</p>
+        <p className="subheading">Selected clients</p>
         <ul className="flex gap-4">
             <li>Big Bank</li>
             <li>Big Retailer</li>
