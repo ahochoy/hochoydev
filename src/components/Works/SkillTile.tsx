1091db17f8f2f7b746d6e5f6036eb12fb4552aff
Andrew HoChoy <a.hochoy@gmail.com>
2023-03-25 18:23:02 -0700

    visual tweaks to text and spacing

--- src/components/Works/SkillTile.tsx
+++ src/components/Works/SkillTile.tsx
@@ -9,12 +9,12 @@ interface SkillTileProps {
 export function SkillTile({ focusArea, blurb, skills}: SkillTileProps) {
 
     const skillsMarkup = skills.map( (skill) => {
-        return <li className="text-sm bg-neutral-100 rounded-full px-3 py-1 w-fit whitespace-nowrap">{skill}</li>
+        return <li className="body-text bg-neutral-100 rounded-full px-3 py-1 w-fit whitespace-nowrap">{skill}</li>
     })
 
     return <div className="w-full space-y-4">
-        <p className="text-2xl font-bold leading-tight tracking-tighter uppercase">{focusArea}</p>
-        <p>{blurb}</p>
+        <p className="heading font-bold leading-tight tracking-tighter uppercase">{focusArea}</p>
+        <p className="subheading">{blurb}</p>
         <ul className="flex gap-2 flex-wrap">{skillsMarkup}</ul>
     </div>
 }
